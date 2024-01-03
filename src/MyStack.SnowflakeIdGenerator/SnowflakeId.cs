80d31c9b34604c6098bde8c4b725dacdfcf90c8a
June <june.it@qq.com>
2024-01-03 17:34:04 +0800

    更改命名

--- src/MyStack.SnowflakeIdGenerator/SnowflakeId.cs
+++ src/MyStack.SnowflakeIdGenerator/SnowflakeId.cs
@@ -1,48 +1,47 @@
 ﻿using Microsoft.Extensions.Options;
 using System;
-using System.Text;
 
 namespace MyStack.SnowflakeIdGenerator
 {
     public class SnowflakeId : ISnowflakeId
     {
-        // 开始时间截((new DateTime(2020, 1, 1, 0, 0, 0, DateTimeKind.Utc)-Jan1st1970).TotalMilliseconds)
+        // The timestamp of the start time
         private const long _twepoch = 1577836800000L;
-        // 机器id所占的位数
+        // The bits of Machine ID
         private const int _machineIdBits = 5;
-        // 数据标识id所占的位数
+        // The bits of Group ID
         private const int _groupIdBits = 5;
-        // 支持的最大机器id，结果是31 (这个移位算法可以很快的计算出几位二进制数所能表示的最大十进制数)
-        private const long _maxWorkerId = -1L ^ (-1L << _machineIdBits);
-        // 支持的最大数据标识id，结果是31
-        private const long _maxDatacenterId = -1L ^ (-1L << _groupIdBits);
-        // 序列在id中占的位数
+        // Max value of Machine ID
+        private const long _maxMachineId = -1L ^ (-1L << _machineIdBits);
+        // Max value of Group ID
+        private const long _maxGroupId = -1L ^ (-1L << _groupIdBits);
+        // The bits of Sequence
         private const int _sequenceBits = 12;
-        // 数据标识id向左移17位(12+5)
+        // GroupId shifted to the left by 17 bits
         private const int _groupIdShift = _sequenceBits + _machineIdBits;
-        // 机器ID向左移12位
+        // MachineId shifted to the left by 12 bits
         private const int _machineIdShift = _sequenceBits;
-        // 时间截向左移22位(5+5+12)
+        // Timestamp shifted to the left by 22 bits
         private const int _timestampLeftShift = _sequenceBits + _machineIdBits + _groupIdBits;
-        // 生成序列的掩码，这里为4095 (0b111111111111=0xfff=4095)
+        // Mask of sequence 
         private const long _sequenceMask = -1L ^ (-1L << _sequenceBits);
         public long _lastTimestamp;
         public long _sequence;
 
         /// <summary>
-        /// 雪花ID
+        /// Initialize a SnowflakeId
         /// </summary>
-        /// <param name="datacenterId">数据中心ID</param>
-        /// <param name="workerId">工作机器ID</param>
+        /// <param name="options">The options</param>
+        /// <exception cref="Exception"></exception>
         public SnowflakeId(IOptions<SnawflakeIdOptions> options)
         {
-            if (options.Value.GroupId > _maxDatacenterId || options.Value.GroupId < 0)
+            if (options.Value.GroupId > _maxGroupId || options.Value.GroupId < 0)
             {
-                throw new Exception(string.Format("Group Id can't be greater than {0} or less than 0", _maxDatacenterId));
+                throw new Exception(string.Format("Group Id can't be greater than {0} or less than 0", _maxGroupId));
             }
-            if (options.Value.MachineId > _maxWorkerId || options.Value.MachineId < 0)
+            if (options.Value.MachineId > _maxMachineId || options.Value.MachineId < 0)
             {
-                throw new Exception(string.Format("Machine Id can't be greater than {0} or less than 0", _maxWorkerId));
+                throw new Exception(string.Format("Machine Id can't be greater than {0} or less than 0", _maxMachineId));
             }
             MachineId = options.Value.GroupId;
             GroupId = options.Value.MachineId;
@@ -69,7 +68,7 @@ namespace MyStack.SnowflakeIdGenerator
                 else if (timestamp == _lastTimestamp)
                 {
                     _sequence = (_sequence + 1) & _sequenceMask;
-                    if (_sequence == 0) 
+                    if (_sequence == 0)
                     {
                         timestamp = GetNextTimestamp(_lastTimestamp);
                     }
@@ -121,6 +120,5 @@ namespace MyStack.SnowflakeIdGenerator
         }
 
         private static readonly DateTime Jan1st1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-        protected SnawflakeIdOptions Options { get; }
     }
 }
